74b6e782c77d73383be93b51e224ad6bfdde01cd
austin zhang <austinzhang1018@gmail.com>
2016-10-03 18:37:52 -0400

    Added classes to test Optical distance sensors and gyro sensors.

---
+++ Avalanche/src/main/java/org/firstinspires/ftc/avalanche/utilities/GyroTester.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.avalanche.utilities;
+
+import android.media.MediaPlayer;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.avalanche.R;
+
+/**
+ * Created by Austin on 9/13/2016.
+ */
+@TeleOp(name = "Gyro Tester", group = "Utilities")
+public class GyroTester extends LinearOpMode {
+
+    ModernRoboticsI2cGyro gyro;
+
+    //Initialize and Map All Hardware
+    private void hardwareMapping() throws InterruptedException {
+        gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+
+        gyro.calibrate();
+
+        long startTime = System.currentTimeMillis();
+
+        while (gyro.isCalibrating()) {
+            telemetry.addData("Calibrating: ", Math.round(((double) (System.currentTimeMillis() - startTime))/1000));
+            telemetry.update();
+
+            Thread.sleep(50);
+            idle();
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        hardwareMapping();
+
+        MediaPlayer ready = MediaPlayer.create(hardwareMap.appContext, R.raw.imready);
+        ready.start();
+
+        waitForStart();
+
+        gyro.resetZAxisIntegrator();
+
+        long startTime = System.currentTimeMillis();
+
+        // Go go gadget robot!
+        while (opModeIsActive()) {
+
+            if (gamepad1.a) {
+                gyro.resetZAxisIntegrator();
+                startTime = System.currentTimeMillis();
+            }
+
+            double secondsPerOneDegreeDrift = -1;
+            double degreesDriftPerSecond = (double) gyro.getHeading() / ((System.currentTimeMillis() - startTime) * 1000);
+
+            if (gyro.getHeading() != 0) {
+                secondsPerOneDegreeDrift = (double) (System.currentTimeMillis() - startTime) / gyro.getHeading() / 1000;
+            }
+
+            telemetry.addData("heading: ", gyro.getHeading());
+            telemetry.addData("drift per second: ", degreesDriftPerSecond);
+            telemetry.addData("seconds per degree drift: ", secondsPerOneDegreeDrift);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
+
+
